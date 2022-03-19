a1e4bf924211850910ecf9b6dac4f55649902af6
OLAGUNDOYE EMMANUEL <olagundoyeemmanuel@OLAGUNDOYEs-MacBook-Pro.local>
2022-03-19 09:30:20 +0100

    feat(progress-bar): add feature to allow progress bar move to 100%

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
-const progress = document.querySelector(".js-completed-bar");
-const numberPercent = document.querySelector(".completed-bar__truck")
+function move() {
+  var elem = document.getElementsByClassName("progess"); 
+  
+  
 
-var percent = 30
+  var width = 50;
+  var id = setInterval(frame, 10);
 
-function complete(){
-    percent = 100
-    console.log(percent)
-}
-if (progress || numberPercent) {
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      for (var i=0; i<elem.length; i++) {
 
-    progress.setAttribute("data-complete", "50")
+        elem[i].style.width =  width + '%'; 
+        elem[i].innerHTML =  width * 1  + '%';
+    
+    }
+     
+     
+    }
+  }}
 
-    numberPercent.innerHTML = progress.getAttribute("data-complete") + "%";
-  progress.style.width = progress.getAttribute("data-complete") + "%";
-  progress.style.opacity = 1;
-}
+  document.getElementById("button").addEventListener("click", move);
 
 
-document.getElementById("button").addEventListener("click", maxProgress);
+// const progress = document.querySelector(".js-completed-bar");
+// const numberPercent = document.querySelector(".completed-bar__truck")
 
-function maxProgress() {
-    progress.setAttribute("data-complete", "100")
+// var percent = 30
 
-    numberPercent.innerHTML = progress.getAttribute("data-complete") + "%";
-    progress.style.width = progress.getAttribute("data-complete") + "%";
-  progress.style.opacity = 1;
-}
+// function complete(){
+//     percent = 100
+//     console.log(percent)
+// }
+// if (progress || numberPercent) {
+
+//     progress.setAttribute("data-complete", "50")
+
+//     numberPercent.innerHTML = progress.getAttribute("data-complete") + "%";
+//   progress.style.width = progress.getAttribute("data-complete") + "%";
+//   progress.style.opacity = 1;
+// }
+
+
+
+// function maxProgress() {
+//     progress.setAttribute("data-complete", "100")
+
+//     numberPercent.innerHTML = progress.getAttribute("data-complete") + "%";
+//     progress.style.width = progress.getAttribute("data-complete") + "%";
+//   progress.style.opacity = 1;
+// }
+
+
